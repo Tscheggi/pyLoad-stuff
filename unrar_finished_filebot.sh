23ddc55dcb4ea30ab3dd7172a9b774f593ddc401
Gutz-Pilz <hannes.mueller87@gmail.com>
2013-11-05 14:38:32 +0100

    Update unrar_finished_filebot.sh
    This is a script to sort the Movies and Series downloaded from pyLoad using the awesome Program FileBot.
    
    Just edit the Global Variables to suite your environment and Copy the file to ~/pyload/scripts/unrar_finished
    
    than activate the "external scripts" plugin in pyLoad and you are ready2go

--- unrar_finished_filebot.sh
+++ unrar_finished_filebot.sh
@@ -1,44 +1,63 @@
 #!/bin/bash
 
 # Global Variables #
-DATE=$(date +%d.%m.%Y\ %H:%M:%S) 
-BaseDir=/media/5a24e136-09b9-48e1-95db-b44d5db3e28a
-DownloadDir=${BaseDir}/Medien/Downloads
-MediaDir=${BaseDir}/Medien
-LogFile=$BaseDir/Scripts/filebot.log
-ExtScript=/root/filebot.sh #to make further execution to the moved file
+
+BaseDir=/media/5a24e136-09b9-48e1-95db-b44d5db3e28a 	# MAINFolder
+MediaDir=${BaseDir}/Medien 				# where the files are moved to (~Mediadir/TV Shows/SeriesName/Season x/SeriesName - SxEx - Title_en ; ~Mediadir/Movies/movie (year))
+LogFile=$BaseDir/Scripts/filebot.log 			# LogFile
+DATE=$(date +%d.%m.%Y\ %H:%M:%S) 			# for a better overview in the logfile
+ExtScript=/root/filebot.sh 				# to make further execution to the moved file
+
+
 
 #Pyload
-DownloadFolder=$MediaDir/$1
-SERVICE=filebot
+# "/$1" is given by pyLoad after a package is extracted succesfully (/Downloads/packagefolder)
+# so just edit MediaDir and BaseDir
+DownloadFolder=$MediaDir/$1 				# The given packagefolder from pyLoad to only process the finished files
+SERVICE=filebot 					# should be no need edit that
+
+
 
 #FileBot-defs
 #MovieFormat is set to rename the files with german titles
+
 MovieFormat="movieFormat=Movies/{net.sourceforge.filebot.WebServices.TMDb.getMovieInfo(movie, Locale.GERMAN).name} {'('+y+')'}/{net.sourceforge.filebot.WebServices.TMDb.getMovieInfo(movie, Locale.GERMAN).name} {'('+y+')'}"
 Ignore="ignore=\b(?i:do(k|c)u)\b"
 Execute="exec=sh $ExtScript \"{file}\""
 Extras="clean=y artwork=y subtitles=de"
 
 
+
 #code
+
 echo -e "##############################" | tee -a $LogFile
 echo -e "$DATE \tINFO:\tpyLoad UNRAR_FINISHED" | tee -a $LogFile
 
-# Am i the only instance?
+
+
+# Am i the only instance - if not cancel
+# to prevent that 2 instances are gonna "fight" each other
+
 if [[ "`pidof -x $(basename $0) -o %PPID`" ]]; then
         echo  -e "$DATE \tWarning:\tThis script already runs with PID `pidof -x $(basename $0) -o %PPID`" | tee -a $LogFile
         exit
 fi
 
+
+
 # Is filebot already running?
+
 if ps ax | grep -v grep | grep -v $0 | grep $SERVICE > /dev/null
 then
 	echo -e "$DATE \tWarning:\t$SERVICE Already running - Abort!" | tee -a $LogFile
 else
 	sleep 5
         echo -e "$DATE \tINFO:\t$SERVICE not running" | tee -a $LogFile
+        
+        
 		
-	# Nur ausführen wenn keine RAR-Dateien da sind - das entpacken soll pyLoad selbst machen(passwörter)?
+	# only run if pyload is done with extracting - pyLoad has to extract because of the Archive passwords
+	
 	cd "$DownloadFolder"
 	count=`ls -1 *.rar 2>/dev/null | wc -l`
 	if [ $count != 0 ]
@@ -46,6 +65,7 @@ else
 		echo -e "$DATE \tWarning:\tThere are Archives... Abort!" | tee -a $LogFile
 	else
 		echo -e "$DATE \tINFO:\tNo RAR-file found in $1  - START FileBot!" | tee -a $LogFile
+		
 		
 		# Functions #
 		sortiere(){
@@ -55,7 +75,7 @@ else
 		filebot -script fn:cleaner "$DownloadFolder" --def root=y "$Ignore" "exts=jpg|nfo|rar|etc" "terms=sample|trailer|etc"
 		}
 		
-		# Execute #
+		# Execute the functions#
 		echo -e "$DATE \tINFO:\tFilebot ausfuehren" | tee -a $LogFile
 		sortiere | tee -a $LogFile
 		echo -e "$DATE \tINFO:\tFilebot aufraeumen" | tee -a $LogFile
