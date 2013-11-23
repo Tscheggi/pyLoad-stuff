a15934dbca7297d79c520164b379e69666024738
Gutz-Pilz <hannes.mueller87@gmail.com>
2013-11-23 15:52:33 +0100

    Update HDareaFetcher.py

--- HDareaFetcher.py
+++ HDareaFetcher.py
@@ -57,7 +57,7 @@ class HDareaFetcher(Hook):
                         rating = rating.replace('-/','0.')
                         list = [self.getConfig("quality")]
                         list2 = ['S0','s0','season','Season','DOKU','doku','Doku']
-                        not any(word in title for word in list) and rating > self.getConfig("rating"):
+                        if any(word in title for word in list) and rating > self.getConfig("rating"):
                             if any (word in title for word in list2):
                                 self.core.log.info("HDArea: rejected, because Series " +title[0:20])
                             else: 
@@ -68,6 +68,6 @@ class HDareaFetcher(Hook):
                         else:
                             if rating < self.getConfig("rating"):
                                 self.core.log.info("HDArea: IMDB-Rating ("+rating+") to low: " +title[0:20])
-                            if not any(word in title for word in list)
+                            if not any(word in title for word in list):
                                 self.core.log.info("HDArea: Quality ("+self.getConfig("quality")+") mismatch: " +title[0:20])
             f.close()
