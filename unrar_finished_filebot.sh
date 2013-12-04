0ecc37994ead5b7dd9cc3130fc68b7d169c13fb8
Gutz-Pilz <hannes.mueller87@gmail.com>
2013-12-04 18:32:20 +0100

    Update unrar_finished_filebot.sh

--- unrar_finished_filebot.sh
+++ unrar_finished_filebot.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Globale Variablen #
+# Global Variables #
 DATE=$(date +%d.%m.%Y\ %H:%M:%S)
 BaseDir=/media/5a24e136-09b9-48e1-95db-b44d5db3e28a
 DownloadDir=${BaseDir}/Medien/Downloads
 MediaDir=${BaseDir}/Medien
 logline=$(date +'%d.%m.%Y')" "$(date +'%H:%M:%S')" FileBot"
-LogFile=/root/.pyload/Logs/log.txt                      # LogFile
+LogFile=/root/.pyload/Logs/log.txt                   
 ExtScript=/root/filebot.sh
 
-#Pyload
+# Pyload
 DownloadFolder=$MediaDir/$1
-SERVICE=filebot
+SERVICE=unrar
 
-#FileBot-defs
+# FileBot-defs
 MovieFormat="movieFormat=Movies/{net.sourceforge.filebot.WebServices.TMDb.getMovieInfo(movie, Locale.GERMAN).name} {'('+y+')'}/{net.sourceforge.filebot.WebServices.TMDb.getMovieInfo(movie, Locale.GERMAN).name} {'('+y+')'}"
 Ignore="ignore=\b(?i:doku)\b"
 Execute="exec=sh $ExtScript \"{file}\""
@@ -22,8 +22,14 @@ Extras="clean=y artwork=n subtitles=de"
 echo -e "$logline ##########################" | tee -a $LogFile
 echo -e "$logline ............unrar_finished" | tee -a $LogFile
 
+        # check if there is a unrar process going on
+        while ps ax | grep -v grep | grep -v $0 | grep $SERVICE > /dev/null
+                do
+                        echo -e "$logline $SERVICE still running ...WAITING..." | tee -a $LogFile
+                        sleep 10
+                done
 
-        # Funktionen #
+        # Functions #
         sortiere(){
         filebot -script fn:amc "$DownloadFolder" --output "$MediaDir" --conflict override -non-strict --action move --def "$MovieFormat" "$Ignore" "$Execute" $Extras
         }
@@ -37,14 +43,13 @@ echo -e "$logline ............unrar_finished" | tee -a $LogFile
         curl -s -d '{"jsonrpc":"2.0","method":"VideoLibrary.Scan","id":1}' -H 'content-type: application/json;' http://192.168.0.107:8585/jsonrpc?VideoLibrary.Scan
         }
 
-                # Ausfuehren #
-                # Execute the functions#
-                echo -e "$logline sorting Files with Filebot" | tee -a $LogFile
-                sortiere
-                echo -e "$logline cleaning Clutter with Filebot" | tee -a $LogFile
-                cleaning
-                #echo -e "$logline XBMC clean" | tee -a $LogFile
-                #xbmc_clean
-                #echo -e "$logline XBMC scan" | tee -a $LogFile
-                #xbmc_scan
-                exit
+        # Execute the functions#
+        echo -e "$logline sorting Files with Filebot" | tee -a $LogFile
+        sortiere
+        echo -e "$logline cleaning Clutter with Filebot" | tee -a $LogFile
+        cleaning
+        #echo -e "$logline XBMC clean" | tee -a $LogFile
+        #xbmc_clean
+        #echo -e "$logline XBMC scan" | tee -a $LogFile
+        #xbmc_scan
+exit
