8a306065879801df4ea046403ec1e87c2f8cb2f5
Gutz-Pilz <hannes.mueller87@gmail.com>
2014-01-03 21:47:48 +0100

    Update filebot.sh
    changed mail thingy and added filsize information

--- filebot.sh
+++ filebot.sh
@@ -12,34 +12,36 @@ LogFile=/root/.pyload/Logs/log.txt
 ## DTS2AC3 um den Datentransfer von OSDrive zu NASDrive zu umgehen (NASHD -> NASHD)
 tmpFolder=$MediaDir/tmp
 
-## String Manipulation für den EmailVersand
+## String Manipulation
 mailtitle_ext=${1##*/}
 mailtitle=${mailtitle_ext%.*}
 
-## nur dateien mit der Endung "mkv" als email versenden
+## nur dateien mit der Endung "mkv"
 if [[ $1 =~ .*mkv.* ]]
 then
 
-  echo  "$logline ##########################" | tee -a $LogFile
-  echo  "$logline Dateihandling nachdem FILEBOT fertig ist" | tee -a $LogFile
-  echo  "$logline Datei wurde nach ~${1##*/Medien} verschoben" | tee -a $LogFile
-  cd /
+FileSize1=$(ls -lah "$1" | awk '{ print $5}')
+echo  "$logline ##########################" | tee -a $LogFile
+echo  "$logline Dateihandling nachdem FILEBOT fertig ist" | tee -a $LogFile
+echo  "$logline Datei wurde nach ~${1%/*.mkv}/* verschoben" | tee -a $LogFile
+cd /
 
-  echo "$logline Entferne andersprachige Tonspur" | tee -a $LogFile
-  python /root/mkv_ger.py "${1%/*.mkv}"
+echo "$logline Entferne andersprachige Tonspur" | tee -a $LogFile
+python /root/mkv_ger.py "${1%/*.mkv}"
 
-  echo "$logline DTS Tracks zu AC3 wandeln" | tee -a $LogFile
-  /mkvdts2ac3/mkvdts2ac3.sh -w "$tmpFolder" -n "$1"
+echo "$logline DTS Tracks zu AC3 wandeln" | tee -a $LogFile
+/mkvdts2ac3/mkvdts2ac3.sh -w "$tmpFolder" -n "$1"
 
-  echo "$logline Neues Erstelldatum" | tee -a $LogFile
-  touch -c "$1"
+echo "$logline Neues Erstelldatum" | tee -a $LogFile
+touch -c "$1"
 
-  echo "$logline CHMOD 777" | tee -a $LogFile
-  chmod 777 "$1"
+echo "$logline CHMOD 777" | tee -a $LogFile
+chmod 777 "$1"
 
-  echo "$logline E-Mail senden" | tee -a $LogFile
-  echo -e "pyLoad hat einen Film heruntergeladen und wurde durch Filebot nach \n\n\t ~${1##*/Medien} \n\n verschoben \n\n\n Sincerly \n your lovely NAS" | mailx -s "INFO: $mailtitle runtergeladen" hannes.mueller87@gmail.com;
+FileSize2=$(ls -lah "$1" | awk '{ print $5}')
 
+echo "$logline E-Mail senden" | tee -a $LogFile
+echo -e "Verschoben nach:\t~${1##*/Medien}\n\nDateigröße vorher:\t$FileSize1\nDateigröße danach:\t$FileSize2\n\n\nSincerly\nyour lovely NAS" | mailx -s "INFO: $mailtitle runtergeladen" hannes.mueller87@gmail.com;
 else
-  echo "$logline $mailtitle_ext ist keine MKV" | tee -a $LogFile
+echo "$logline $mailtitle_ext ist keine MKV" | tee -a $LogFile
 fi
